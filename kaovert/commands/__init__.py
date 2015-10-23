afa18329bb641e2029148f1d92d6d777a79c9431
Chris Loew <cloew123@gmail.com>
2015-10-23 12:12:08 -0600

    Added command to List the Subtitles for an MKV file

--- kaovert/commands/__init__.py
+++ kaovert/commands/__init__.py
@@ -1,3 +1,4 @@
 from kao_command import Commands
 
-commands = Commands(__name__, {'audio':'list_audio_tracks.ListAudioTracks'})
+commands = Commands(__name__, {'audio':'list_audio_tracks.ListAudioTracks',
+                               'subtitles':'list_subtitle_tracks.ListSubtitleTracks'})
--- kaovert/commands/list_subtitle_tracks.py
+++ kaovert/commands/list_subtitle_tracks.py
@@ -1,9 +1,9 @@
 from kao_command.args import Arg
 import enzyme
 
-class ListAudioTracks:
-    """ Represents a command to list the Audio Tracks for a file """
-    description = "List Audio Tracks"
+class ListSubtitleTracks:
+    """ Represents a command to list the Subtitle Tracks for a file """
+    description = "List Subtitle Tracks"
     args = [Arg('filename', action='store', help='MKV file to inspect')]
         
     def run(self, *, filename):
@@ -11,5 +11,5 @@ class ListAudioTracks:
         with open(filename, 'rb') as f:
             mkv = enzyme.MKV(f)
             
-        for i, track in enumerate(mkv.audio_tracks):
+        for i, track in enumerate(mkv.subtitle_tracks):
             print("{0}: {1}".format(i+1, track.language))
