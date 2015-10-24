086f200583e980b7fd943f201dbd1ebd18769f80
Chris Loew <cloew123@gmail.com>
2015-10-24 16:58:51 -0600

    Fixed some bugs and added some missed requirements

---
+++ kaovert/config/__init__.py
@@ -0,0 +1,1 @@
+from .conversion_config import ConversionConfig
--- kaovert/config/conversion_config.py
+++ kaovert/config/conversion_config.py
@@ -1,7 +1,11 @@
-from kao_decorators import lazy_property
+from kao_decorators import lazy_property, proxy_for
 from kao_toml import KaoToml
 
+CONFIG_ATTRS = {'output'}
+
+
 @proxy_for('_toml', ['save'])
+@proxy_for('_toml', CONFIG_ATTRS)
 class ConversionConfig:
     """ Represents a Conversion Configuration """
     
