cca8eeda03d00548f482b28360c70e46a31fbd3e
Chris Loew <cloew123@gmail.com>
2015-10-25 14:01:02 -0600

    Added proper adding of audio streams

--- kaovert/config/toml_attrs.py
+++ kaovert/config/toml_attrs.py
@@ -1,3 +1,4 @@
+from kao_dict import KaoDict
 
 def toml_attrs(*attrs):
     """ Add properties for the attrs to provide an interface based on the contents of TOML file """
@@ -13,3 +14,20 @@ def toml_attrs(*attrs):
             add_property(cls, attr)
         return cls
     return addAttrData
+    
+def toml_group(*groups):
+    """ Add properties for the groups to provide an interface based on the contents of TOML file """
+    def addGroupData(cls):
+        def add_property(cls, group):
+            def setter(self, v):
+                setattr(self._toml, group, v)
+            def getter(self):
+                if group not in self._toml:
+                    setattr(self._toml, group, KaoDict())
+                return getattr(self._toml, group)
+            setattr(cls, group, property(getter, setter))
+            
+        for group in groups:
+            add_property(cls, group)
+        return cls
+    return addGroupData
