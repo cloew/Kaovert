fe6d3dd1abd5c27d9187df06320cbe7e769779e4
Chris Loew <cloew123@gmail.com>
2015-10-28 14:30:12 -0600

    Added some parsing of the setup script to allow it to install on cygwin

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
-from cx_Freeze import setup, Executable
-from enzyme.parsers import ebml
-import os
+try:
+    from cx_Freeze import setup, Executable
+    from enzyme.parsers import ebml
+    import os
+    
+    kwargs = {'options':{"build_exe": {'packages':['kaovert'],
+                                       'zip_includes':[(os.path.join(os.path.dirname(ebml.__file__), "specs/matroska.xml"), "enzyme/parsers/ebml/specs/matroska.xml")]}},
+              'executables':[Executable('kaovert/scripts/kaovert')]
+             }
+    
+except ImportError:
+    from distutils.core import setup
+    kwargs = {}
 
 setup(name='kaovert',
       version='0.1.0',
@@ -8,13 +18,12 @@ setup(name='kaovert',
       author='Chris Loew',
       author_email='cloew123@gmail.com',
       packages=['kaovert',
+                'kaovert.args',
                 'kaovert.commands',
                 'kaovert.config',
+                'kaovert.config.flex',
                 'kaovert.conversion',
-                'kaovert.conversion.hbclis',
-                'kaovert.config.flex'],
-      options = {"build_exe": {'packages':['kaovert'],
-                               'zip_includes':[(os.path.join(os.path.dirname(ebml.__file__), "specs/matroska.xml"), "enzyme/parsers/ebml/specs/matroska.xml")]}},
-      executables = [Executable('kaovert/scripts/kaovert')],
-      scripts=['kaovert/scripts/kaovert']
+                'kaovert.conversion.hbclis'],
+      scripts=['kaovert/scripts/kaovert'],
+      **kwargs
      )
