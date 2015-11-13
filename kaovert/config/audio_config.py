7b556897e31d3903983e31b454985c773abbcf48
Chris Loew <cloew123@gmail.com>
2015-11-13 08:12:28 -0700

    Added Config Class to represent the audio stream

--- kaovert/config/audio_config.py
+++ kaovert/config/audio_config.py
@@ -1,6 +1,7 @@
-from flexconfig import Config, FlexAttr, WrappedAttr
+from .audio_stream_config import AudioStreamConfig
+from flexconfig import Config, FlexAttr, WrappedAttr, WrapperList
 
 class AudioConfig(Config):
     """ Represents the Audio Config for a Conversion Configuration """
     includeAll = FlexAttr('includeAll')
-    streams = WrappedAttr('streams', list)
+    streams = WrappedAttr('streams', WrapperList, kwargs={'wrapperCls':AudioStreamConfig})
---
+++ kaovert/config/audio_stream_config.py
@@ -0,0 +1,5 @@
+from flexconfig import Config, FlexAttr
+
+class AudioStreamConfig(Config):
+    """ Represents an Audio Stream Configuration """
+    number = FlexAttr('number')
